a0c1ea0cf988f15f670577678d7289785eb0972a
shayanbalochofficial <144522960+shayanbalochofficial@users.noreply.github.com>
2024-04-17 23:49:41 +0500

    Initial commit

---
+++ SOL.ts
@@ -0,0 +1,22 @@
+// Stages of Life
+
+let age: number = 99;
+
+if (age < 2) {
+    console.log("You are a baby");
+}
+else if (age >= 2 && age < 4) {
+    console.log("You are a toddler");
+}
+else if (age >= 4 && age < 13) {
+    console.log("You are a Kid");
+}
+else if (age >= 13 && age < 20) {
+    console.log("You are a teenager");
+}
+else if (age >= 20 && age < 65) {
+    console.log("You are a Adult");
+}
+else if (age > 65) {
+    console.log("You are an elder");
+}
